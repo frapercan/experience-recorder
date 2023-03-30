36f500c658d87d49ff7baf372766e5c7841fa91c
frapercan <frapercan1@alum.us.es>
2023-03-30 13:46:41 +0200

    docs + tests

---
+++ experience_recorder/main.py
@@ -0,0 +1,19 @@
+
+import yaml
+
+from experience_recorder.configuration.configuration import Configuration
+from experience_recorder.recorder.recorder import Recorder
+import os
+
+if __name__ == "__main__":
+    with open("conf/global.yaml", 'r', encoding='utf-8') as global_configuration:
+        global_configuration = yaml.safe_load(global_configuration)
+        task_configuration = Configuration(global_configuration)
+        task_configuration.save_configuration()
+        recorder = Recorder(global_configuration, task_configuration.conf)
+        recorder.empty_buffer()
+
+
+
+        recorder.start_senses()
+        recorder.start()
