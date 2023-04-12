691e75ffd7e7377dec5be605678ab19e86490661
frapercan <frapercan1@alum.us.es>
2023-04-12 14:30:01 +0200

    readme.md writing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_title = 'Experience Recorder'
 
-autodoc_mock_imports = ["paddleocr"]
+autodoc_mock_imports = ["paddleocr","pynput"]
 master_doc = 'index'
 
 html_favicon = 'favicon.ico'
